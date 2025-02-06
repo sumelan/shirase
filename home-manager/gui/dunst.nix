4527ed0f4eafa91e7aeef67ad3d0aa9a60027977
sumelan <bathys@proton.me>
2025-02-06 21:51:11 +0900

    ...

---
+++ home-manager/gui/dunst.nix
@@ -0,0 +1,38 @@
+{ lib, ... }:
+{
+  services.dunst = {
+    enable = true;
+    settings = {
+      global = {
+        browser = "brave -new-tab";
+        corner_radius = 8;
+        dmenu = "rofi -p dunst:";
+        enable_recursive_icon_lookup = true;
+        ellipsize = "end";
+        follow = "mouse";
+        frame_width = 0;
+        horizontal_padding = 10;
+        icon_path = lib.mkForce "";
+        max_icon_size = 72;
+        mouse_left_click = "do_action";
+        mouse_middle_click = "do_action";
+        mouse_right_click = "close_current";
+        separator_height = 1;
+        show_indicators = "no";
+      };
+
+      urgency_critical = {
+        timeout = 0;
+      };
+
+      urgency_low = {
+        timeout = 10;
+      };
+
+      urgency_normal = {
+        timeout = 10;
+      };
+    };
+  };
+}
+
