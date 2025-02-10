abb4b1906ba3944f6ebf7e96984cdcdf52d1bf71
Su melan <bathys@proton.me>
2025-02-10 10:42:07 +0900

    Update btrfs.nix

--- system/disk/btrfs.nix
+++ system/disk/btrfs.nix
@@ -29,6 +29,7 @@ in
         "/boot" = {
           device = "/dev/disk/by-label/NIXBOOT";
           fsType = "vfat";
+          options = [ "fmask=0022" "dmask=0022" ];
         };
 
         "/nix" = {
