fa9e23eec6a92df9631a8bcec07657b5afaa1511
Sumelan <bathys@proton.me>
2025-02-21 22:09:30 +0900

    remove stylix in nvf

--- home-manager/editor/neovim/default.nix
+++ home-manager/editor/neovim/default.nix
@@ -1,11 +1,4 @@
-{
-  config,
-  pkgs,
-  ...
-}:
-let
-  inherit (config.lib.stylix) colors;
-in
+{ pkgs, ... }:
 {
   imports = [
     ./keymaps.nix
@@ -21,12 +14,8 @@ in
       vim.lsp.enable = true;
       vim.theme = {
         enable = true;
-        name = "base16";
-        style = "dark";
-        base16-colors = {
-          inherit (colors) base00 base01 base02 base03 base04 base05 base06 base07;
-          inherit (colors) base08 base09 base0A base0B base0C base0D base0E base0F;
-        };
+        name = "catppuccin";
+        style = "darker";
         transparent = false;
       };
 
