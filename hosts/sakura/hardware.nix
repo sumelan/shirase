7b8fcd406d9bc552efbe61371a2862258fe7d381
Sumelan <bathys@proton.me>
2025-02-22 21:10:51 +0900

    mv scripts

--- hosts/sakura/hardware.nix
+++ hosts/sakura/hardware.nix
@@ -21,6 +21,7 @@
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
