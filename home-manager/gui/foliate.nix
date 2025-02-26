51f18720ca8ba30641f3736f0ade348eed00f5ab
Sumelan <bathys@proton.me>
2025-02-26 09:10:16 +0900

    add foliate cache in persist

--- home-manager/gui/foliate.nix
+++ home-manager/gui/foliate.nix
@@ -11,5 +11,11 @@
 
   config = lib.mkIf config.custom.foliate.enable {
     home.packages = [ pkgs.foliate ];
+
+    custom.persist = {
+      home.directories = [
+      ".cache/com.github.johnfactotum.Foliate"
+      ];
+    };
   };
 }
