be6feb5e4b6ec5c488cd763429d6e3f5a2aad0bd
Sumelan <bathys@proton.me>
2025-03-02 22:00:49 +0900

    change ssh passwd false

--- system/startup/auth.nix
+++ system/startup/auth.nix
@@ -11,7 +11,7 @@ lib.mkMerge [
       enable = true;
       # disable password auth
       settings = {
-        PasswordAuthentication = true;
+        PasswordAuthentication = false;
         KbdInteractiveAuthentication = false;
       };
     };
