05b437fd6020544f833e35447ca865c21ee7e227
Sumelan <bathys@proton.me>
2025-04-10 00:08:46 +0900

    flake.lock update

--- hosts/sakura/home.nix
+++ hosts/sakura/home.nix
@@ -2,28 +2,15 @@ _: {
   monitors = {
     "HDMI-A-1" = {
       isMain = true;
-      scale = 1.25;
+      scale = 1.00;
       mode = {
-        width = 3840;
-        height = 2160;
-        refresh = 60.000;
+        width = 2560;
+        height = 1440;
+        refresh = 59.951;
       };
       position = {
         x = 0;
         y = 0;
-      };
-      rotation = 0;
-    };
-    "HDMI-A-2" = {
-      scale = 1.25;
-      mode = {
-        width = 2560;
-        height = 1600;
-        refresh = 60.000;
-      };
-      position = {
-        x = 1843;
-        y = 1728;
       };
       rotation = 0;
     };
@@ -40,5 +27,4 @@ _: {
       preset = "Bass Enhancing + Perfect EQ";
     };
   };
-
 }
