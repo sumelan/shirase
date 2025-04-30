d72c203156ffb13400f58bf13688238b69f1219a
Sumelan <bathys@proton.me>
2025-04-30 15:23:17 +0900

    disable tlp

--- hosts/acer/default.nix
+++ hosts/acer/default.nix
@@ -24,16 +24,6 @@
       percentageAction = 3;
       criticalPowerAction = "PowerOff";
     };
-    tlp.settings = {
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-
-      CPU_BOOST_ON_AC = 1;
-      CPU_BOOST_ON_BAT = 0;
-
-      CPU_HWP_DYN_BOOST_ON_AC = 1;
-      CPU_HWP_DYN_BOOST_ON_BAT = 0;
-    };
 
     # touchpad support
     libinput.enable = true;
