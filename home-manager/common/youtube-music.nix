fdbdd8a6cc245300998c576c0a141d76630ebaed
Sumelan <bathys@proton.me>
2025-05-31 00:48:49 +0900

    enable japanese input in discord and youtube-music

--- home-manager/common/youtube-music.nix
+++ home-manager/common/youtube-music.nix
@@ -1,23 +1,27 @@
 { config, pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    playerctl
-    youtube-music
-  ];
+  home.packages = [
+    pkgs.playerctl
+    (pkgs.callPackage pkgs.symlinkJoin {
+      name = "youtube-music";
+      paths = [
+        pkgs.youtube-music
+      ];
 
-  xdg.desktopEntries = {
-    "com.github.th_ch.youtube_music" = {
-      name = "YouTube Music";
-      icon = "youtube-music";
-      exec = "youtube-music %U --enable-wayland-ime --wayland-text-input-version=3";
-      categories = [ "AudioVideo" ];
-      type = "Application";
-    };
-  };
+      buildInputs = [
+        pkgs.makeWrapper
+      ];
+
+      postBuild = ''
+        wrapProgram $out/bin/youtube-music \
+          --add-flags '--enable-wayland-ime --wayland-text-input-version=3'
+      '';
+    })
+  ];
 
   programs.niri.settings.binds = with config.lib.niri.actions; {
     "Mod+Y" = {
-      action = spawn "youtube-music" "%U" "--enable-wayland-ime" "--wayland-text-input-version=3";
+      action = spawn "youtube-music";
       hotkey-overlay.title = "YouTube Music";
     };
   };
