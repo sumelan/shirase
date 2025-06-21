ea6854c73c90f34b3c87691d5981bf47ae01e442
Sumelan <bathys@proton.me>
2025-06-21 18:03:09 +0900

    fix function error
    Signed-off-by: Sumelan <bathys@proton.me>

--- home-manager/common/desktop/niri/idle.nix
+++ home-manager/common/desktop/niri/idle.nix
@@ -34,35 +34,32 @@
             after_sleep_cmd = "niri msg action power-on-monitors";
           };
 
-        listener =
-          [
-            {
-              timeout = 60 * 5;
-              # set monitor backlight to minomum, avoid 0 on OLED monitor.
-              on-timeout = "brightnessctl -s set 5";
-              on-resume = "brightnessctl -r"; # monitor backlight restor.
-            }
+        listener = [
+          {
+            timeout = 60 * 5;
+            # set monitor backlight to minomum, avoid 0 on OLED monitor.
+            on-timeout = "brightnessctl -s set 5";
+            on-resume = "brightnessctl -r"; # monitor backlight restor.
+          }
 
-            {
-              timeout = 60 * 8;
-              # lock screen when timeout has passed.
-              on-timeout = "loginctl lock-session";
-            }
+          {
+            timeout = 60 * 8;
+            # lock screen when timeout has passed.
+            on-timeout = "loginctl lock-session";
+          }
 
-            {
-              timeout = 60 * 10;
-              # screen off when timeout has passed.
-              on-timeout = "niri msg action power-off-monitors";
-              # screen on when activity is detected after timeout has fired.
-              on-resume = "niri msg action power-on-monitors";
-            }
-          ]
-          ++ lib.optionalAttrs isLaptop [
-            {
-              timeout = 60 * 15;
-              on-timeout = "systemctl suspend"; # suspend pc.
-            }
-          ];
+          {
+            timeout = 60 * 10;
+            # screen off when timeout has passed.
+            on-timeout = "niri msg action power-off-monitors";
+            # screen on when activity is detected after timeout has fired.
+            on-resume = "niri msg action power-on-monitors";
+          }
+          (lib.mkIf isLaptop {
+            timeout = 60 * 15;
+            on-timeout = "systemctl suspend"; # suspend pc.
+          })
+        ];
       };
     };
     systemd.user.services.hypridle = {
