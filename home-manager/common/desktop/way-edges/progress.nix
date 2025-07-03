dd1f59b67262294d8ad494b9e184fa94af3bfe00
sumelan <bathys@proton.me>
2025-07-03 23:52:44 +0900

    fix way-edges
    Signed-off-by: sumelan <bathys@proton.me>

--- home-manager/common/desktop/way-edges/progress.nix
+++ home-manager/common/desktop/way-edges/progress.nix
@@ -1,39 +1,12 @@
 {
   lib,
   config,
-  pkgs,
   ...
 }:
 with config.lib.stylix.colors.withHashtag;
 let
   left-click = 272;
   right-click = 273;
-
-  powerCmd = pkgs.writers.writeFish "select_power_setting" ''
-    set APP_NAME "Power Selector"
-
-    function notify -a 1 2
-        ${lib.getExe' pkgs.libnotify "notify-send"} -a "$APP_NAME" -i "battery" "$1" "$2"
-    end
-
-    set choices " Power-saver
-     Balanced
-     Performance"
-
-    set choice (echo -en $choices | fuzzel --dmenu --prompt "󱉓? " --placeholder "Select Power Profile..." --lines 3)
-
-    switch (string split -f 2 " " $choice)
-        case Power-saver
-            powerprofilesctl set power-saver
-            notify " Power-saver" "Profile switched"
-        case Balanced
-            powerprofilesctl set balanced
-            notify " Balanced" "Profile switched"
-        case Performance
-            powerprofilesctl set performance
-            notify " Performance" "Profile switched"
-    end
-  '';
 
   commonConfig = preview: {
     namespace = "progress";
@@ -88,7 +61,7 @@ let
       update-command = "show_battery_progress";
       on-change-command = "";
       event-map = {
-        ${builtins.toString left-click} = powerCmd;
+        ${builtins.toString left-click} = "fuzzel-power";
       };
     };
   };
