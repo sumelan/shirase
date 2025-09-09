6d696759e44ff8d498fb5bbd09b2f8811ebbeeb6
sumelan <sumelan@proton.me>
2025-09-10 02:06:24 +0900

    remove colloid packages
    Signed-off-by: sumelan <sumelan@proton.me>

--- packages/default.nix
+++ packages/default.nix
@@ -3,9 +3,6 @@
   inputs,
   ...
 }: {
-  colloid-pastel-cursors = pkgs.callPackage ./colloid-pastel-cursors {};
-  colloid-pastel-icons = pkgs.callPackage ./colloid-pastel-icons {};
-
   nvf = pkgs.callPackage (
     {
       host ? "acer",
