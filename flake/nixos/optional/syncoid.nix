3143a8d0ad542ea31a5f4b2eaece60de3de3eb5d
sumelan <sumelan@proton.me>
2025-09-24 16:08:30 +0900

    add notify service when syncoid fails
    Signed-off-by: sumelan <sumelan@proton.me>

--- flake/nixos/optional/syncoid.nix
+++ flake/nixos/optional/syncoid.nix
@@ -1,12 +1,17 @@
 {
   lib,
   config,
+  pkgs,
+  user,
   ...
 }: let
   inherit
     (lib)
     mkEnableOption
     mkIf
+    flip
+    mapAttrs'
+    nameValuePair
     ;
 in {
   options.custom = {
@@ -31,6 +36,30 @@ in {
       # 23:50 daily
       interval = "*-*-* 23:50:00";
     };
+
+    # notify when syncoid fails
+    systemd.services =
+      {
+        "notify-problems@" = {
+          enable = true;
+          serviceConfig.User = user;
+          environment.SERVICE = "%i";
+          script = ''
+            export $(cat /proc/$(${pkgs.procps}/bin/pgrep "gnome-session" -u "$USER")/environ |grep -z '^DBUS_SESSION_BUS_ADDRESS=')
+            ${pkgs.libnotify}/bin/notify-send -u critical "$SERVICE FAILED!" "Run journalctl -u $SERVICE for details"
+          '';
+        };
+      }
+      // flip mapAttrs' config.services.syncoid.commands (
+        name: _value:
+          nameValuePair "syncoid-${name}" {
+            unitConfig.OnFailure = "notify-problems@%i.service";
+            # waiting for internet after resume-from-suspend
+            preStart = lib.mkBefore ''
+              until /run/wrappers/bin/ping google.com -c1 -q >/dev/null; do :; done
+            '';
+          }
+      );
 
     # persist syncoid .ssh
     # For syncoid to be able to create /var/lib/syncoid/.ssh/ and to use custom ssh_config or known_hosts.
