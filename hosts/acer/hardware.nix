dacf75947bda18d844d84fa5004d349d0893957c
sumelan <sumelan@proton.me>
2025-11-04 22:52:46 +0900

    add hardware acer
    Signed-off-by: sumelan <sumelan@proton.me>

--- hosts/acer/hardware.nix
+++ hosts/acer/hardware.nix
@@ -12,10 +12,92 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "rtsx_usb_sdmmc"];
+  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "rtsx_usb_sdmmc"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/146410bc-57e5-4ac2-ac4a-7d48cbdf97ca";
+    fsType = "btrfs";
+    options = [
+      "subvol=root"
+      "compress=zstd"
+      "noatime"
+    ];
+  };
+
+  boot.initrd.luks.devices."enc".device = "/dev/disk/by-uuid/c5f1e957-0997-4f8a-8a1f-c6a40b9381b7";
+
+  fileSystems."/nix" = {
+    device = "/dev/disk/by-uuid/146410bc-57e5-4ac2-ac4a-7d48cbdf97ca";
+    fsType = "btrfs";
+    options = [
+      "subvol=nix"
+      "compress=zstd"
+      "noatime"
+    ];
+  };
+
+  fileSystems."/persist" = {
+    device = "/dev/disk/by-uuid/146410bc-57e5-4ac2-ac4a-7d48cbdf97ca";
+    fsType = "btrfs";
+    options = [
+      "subvol=persist"
+      "compress=zstd"
+      "noatime"
+    ];
+    neededForBoot = true;
+  };
+
+  fileSystems."/cache" = {
+    device = "/dev/disk/by-uuid/146410bc-57e5-4ac2-ac4a-7d48cbdf97ca";
+    fsType = "btrfs";
+    options = [
+      "subvol=cache"
+      "compress=zstd"
+      "noatime"
+    ];
+    neededForBoot = true;
+  };
+
+  fileSystems."/snapshots" = {
+    device = "/dev/disk/by-uuid/146410bc-57e5-4ac2-ac4a-7d48cbdf97ca";
+    fsType = "btrfs";
+    options = [
+      "subvol=snapshots"
+      "compress=zstd"
+      "noatime"
+    ];
+    neededForBoot = true;
+  };
+
+  fileSystems."/var/log" = {
+    device = "/dev/disk/by-uuid/146410bc-57e5-4ac2-ac4a-7d48cbdf97ca";
+    fsType = "btrfs";
+    options = [
+      "subvol=log"
+      "compress=zstd"
+      "noatime"
+    ];
+    neededForBoot = true;
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/E623-1BA1";
+    fsType = "vfat";
+    options = ["fmask=0022" "dmask=0022"];
+  };
+
+  swapDevices = [];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp2s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
--- modules/nixos/common/default.nix
+++ modules/nixos/common/default.nix
@@ -3,7 +3,6 @@ _: {
     ./auth.nix
     ./boot.nix
     ./config.nix
-    ./filesystem.nix
     ./greetd.nix
     ./impermanence.nix
     ./niri.nix
