c48ac821eae0416fecfbe5f8c6bde4b8b4593e18
sumelan <sumelan@proton.me>
2025-12-01 02:20:22 +0900

    small change
    Signed-off-by: sumelan <sumelan@proton.me>

--- modules/home-manager/common/mpd.nix
+++ modules/home-manager/common/mpd.nix
@@ -8,11 +8,11 @@
     "d! %t/mpd - ${user} users - -"
   ];
 
-  services = {
-    mpd = let
-      cfg = config.services.mpd;
-      runtimeDir = "/run/user/1000/mpd";
-    in {
+  services = let
+    cfg = config.services.mpd;
+    runtimeDir = "/run/user/1000/mpd";
+  in {
+    mpd = {
       enable = true;
       musicDirectory = config.xdg.userDirs.music;
       dataDir = "${config.xdg.dataHome}/mpd";
@@ -31,13 +31,14 @@
     # mpd to mpris2 bridge
     mpdris2 = {
       enable = true;
-      notifications = true; # enable song change notifications
+      # enable song change notifications
+      notifications = true;
     };
 
     mpd-discord-rpc = {
       enable = true;
       settings = {
-        hosts = [config.services.mpd.network.listenAddress];
+        hosts = [cfg.network.listenAddress];
         format = {
           details = "$title";
           state = "On $album by $artist";
