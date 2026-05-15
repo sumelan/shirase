57a3e709e082deafe4b0373a25de145220c7ca45
sumelan <sumelan@proton.me>
2026-05-16 03:47:11 +0900

    chore: update inputs and fetch packages

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -44,10 +44,10 @@
   };
   helium = {
     pname = "helium";
-    version = "0.12.1.1";
+    version = "0.12.3.1";
     src = fetchurl {
-      url = "https://github.com/imputnet/helium-linux/releases/download/0.12.1.1/helium-0.12.1.1-x86_64.AppImage";
-      sha256 = "sha256-+UE+JqQtxbA5szPvAohapXlES21VBOdNsV6Ej1dRRfs=";
+      url = "https://github.com/imputnet/helium-linux/releases/download/0.12.3.1/helium-0.12.3.1-x86_64.AppImage";
+      sha256 = "sha256-VnOhzhAulvFNBB/0AD1d+K/TzfFL9Zwtk/vcm5vWl+I=";
     };
   };
 }
