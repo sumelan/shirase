8f8f15f5e8a0c5e27c292b1182f9767953708e94
sumelan <sumelan@proton.me>
2026-05-18 18:12:51 +0900

    fix ssh
    Signed-off-by: sumelan <sumelan@proton.me>

--- modules/user/ssh/config.nix
+++ modules/user/ssh/config.nix
@@ -5,6 +5,11 @@ _: {
         HostName 192.168.68.62
         Port 22
         User sumelan
+
+        # Prevent using ssh-agent or another keyfile,
+        # useful for testing
+        IdentitiesOnly yes
+        IdentityFile ~/.ssh/sakura
     '';
   };
 }
