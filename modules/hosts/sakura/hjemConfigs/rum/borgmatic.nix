943cb95b1b28b69e8ee305221bacea31fe52fa29
sumelan <sumelan@proton.me>
2026-09-01 21:10:24 +0900

    fix borgmatic
    Signed-off-by: sumelan <sumelan@proton.me>

--- modules/hosts/sakura/hjemConfigs/rum/borgmatic.nix
+++ modules/hosts/sakura/hjemConfigs/rum/borgmatic.nix
@@ -4,7 +4,41 @@
     pkgs,
     user,
     ...
-  }: {
+  }: let
+    conf = {
+      checks = [
+        {
+          name = "repository";
+          frequency = "2 weeks";
+        }
+        {
+          name = "archives";
+          frequency = "4 weeks";
+        }
+        {
+          name = "data";
+          frequency = "6 weeks";
+        }
+        {
+          name = "extract";
+          frequency = "6 weeks";
+        }
+      ];
+
+      keep_daily = 7;
+      keep_weekly = 4;
+      keep_monthly = 6;
+
+      ssh_command = "ssh -i /home/sumelan/.ssh/borgbase";
+      encryption_passcommand =
+        # sh
+        ''${lib.getExe pkgs.bitwarden-cli} get password Borgmatic --session 85iGC/4x6Uaj0BFy2wGOhteaK4uTKLmsC/LD9sAlM3kgKoADtlbHqxftw1Zef9RVgJb3GXYy2GSCSa+BsOGP2w=='';
+
+      zfs = {
+        zfs_command = lib.getExe config.boot.zfs.package;
+      };
+    };
+  in {
     hjem.users.${user}.rum = {
       services.borgmatic = {
         enable = true;
@@ -15,92 +49,36 @@
         };
 
         configurations = {
-          home = {
-            source_directories = [
-              "/persist/home/sumelan/Documents"
-              "/persist/home/sumelan/Music"
-              "/persist/home/sumelan/Pictures"
-              "/persist/home/sumelan/Videos"
-            ];
-            repositories = [
-              {
-                label = "borgbase";
-                path = "ssh://whcwf2xd@whcwf2xd.repo.borgbase.com/./repo";
-              }
-            ];
-
-            checks = [
-              {
-                name = "repository";
-                frequency = "2 weeks";
-              }
-              {
-                name = "archives";
-                frequency = "4 weeks";
-              }
-              {
-                name = "data";
-                frequency = "6 weeks";
-              }
-              {
-                name = "extract";
-                frequency = "6 weeks";
-              }
-            ];
-
-            keep_daily = 7;
-            keep_weekly = 4;
-            keep_monthly = 6;
-
-            ssh_command = "ssh -i /home/sumelan/.ssh/borgbase";
-            encryption_passcommand =
-              # sh
-              ''${lib.getExe pkgs.bitwarden-cli} get password Borgmatic --session 2bQ7xQ4SpZ1e0084VdX/PCzwqiyZ0Ase6xmg8g/8kFlS6MU/En7qPwNP2DRDsx8m4HBsQ/RiFdy++i0fxmw30g=='';
-
-            zfs = {
-              zfs_command = lib.getExe config.boot.zfs.package;
+          home =
+            conf
+            // {
+              source_directories = [
+                "/persist/home/sumelan/Documents"
+                "/persist/home/sumelan/Music"
+                "/persist/home/sumelan/Pictures"
+                "/persist/home/sumelan/Videos"
+              ];
+              repositories = [
+                {
+                  label = "borgbase";
+                  path = "ssh://whcwf2xd@whcwf2xd.repo.borgbase.com/./repo";
+                }
+              ];
             };
-          };
 
-          audiobookshelf = {
-            source_directories = ["/persist/var/lib/audiobookshelf"];
-            repositories = [
-              {
-                label = "borgbase";
-                path = "ssh://ynlt03ko@ynlt03ko.repo.borgbase.com/./repo";
-              }
-            ];
-
-            checks = [
-              {
-                name = "repository";
-                frequency = "2 weeks";
-              }
-              {
-                name = "archives";
-                frequency = "4 weeks";
-              }
-              {
-                name = "data";
-                frequency = "6 weeks";
-              }
-              {
-                name = "extract";
-                frequency = "6 weeks";
-              }
-            ];
-
-            keep_daily = 7;
-            keep_weekly = 4;
-            keep_monthly = 6;
-
-            ssh_command = "ssh -i /home/sumelan/.ssh/borgbase";
-            encryption_passcommand = "${lib.getExe pkgs.bitwarden-cli} get password Borgmatic --session cbv3d7IVNpJVZpQN5+VDIqVvNhpeDiUDBog2mGdtnMz2ZOJY5eg27UPCaUUuaQNenH/L0UudNVI8FM1/uBDodA=";
-
-            zfs = {
-              zfs_command = lib.getExe config.boot.zfs.package;
+          audiobookshelf =
+            conf
+            // {
+              source_directories = [
+                "/persist/var/lib/audiobookshelf"
+              ];
+              repositories = [
+                {
+                  label = "borgbase";
+                  path = "ssh://ynlt03ko@ynlt03ko.repo.borgbase.com/./repo";
+                }
+              ];
             };
-          };
         };
       };
     };
