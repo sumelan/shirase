80f3f275eb0354d4155eab07d42a8fe7ebdaf39c
sumelan <sumelan@proton.me>
2026-09-10 20:20:19 +0900

    change minibookx widget
    Signed-off-by: sumelan <sumelan@proton.me>

--- modules/hosts/minibookx/hjemConfigs/noctalia.nix
+++ modules/hosts/minibookx/hjemConfigs/noctalia.nix
@@ -24,6 +24,9 @@ _: {
                 cx = 1784.0;
                 cy = 1112.0;
                 output = "DSI-1";
+                placement_height = 1200.0;
+                placement_width = 1920.0;
+
                 rotation = 0.0;
                 type = "label";
 
@@ -33,7 +36,7 @@ _: {
                   color = "hover";
                   description = "";
                   font_family = config.custom.fonts.monospace;
-                  opacity = 0.5;
+                  opacity = 0.45;
                   shadow = true;
                   title = " NixOS";
                 };
@@ -82,40 +85,50 @@ _: {
               "lockscreen-login-box@DSI-1" = {
                 box_height = 70.0;
                 box_width = 400.0;
-                cx = 960.0;
-                cy = 1081.0;
+                cx = 1584.0;
+                cy = 1061.0;
                 output = "DSI-1";
+                placement_height = 1200.0;
+                placement_width = 1920.0;
+
                 rotation = 0.0;
                 type = "login_box";
 
                 settings = {
                   background_color = "on_tertiary";
-                  background_opacity = 0.88;
+                  background_opacity = 0.75;
                   background_radius = 12.0;
                   center_password_text = false;
                   input_opacity = 1.0;
                   input_radius = 6.0;
+                  layout = "compact";
                   show_caps_lock = true;
                   show_keyboard_layout = true;
                   show_login_button = true;
+                  show_media = true;
+                  show_session_buttons = true;
+                  show_unlock_hint = true;
+                  show_weather = true;
                 };
               };
 
               lockscreen-widget-0000000000000001 = {
-                box_height = 176.0;
-                box_width = 400.0;
-                cx = 960.0;
-                cy = 368.0;
+                box_height = 160.0;
+                box_width = 384.0;
+                cx = 1584.0;
+                cy = 680.0;
                 output = "DSI-1";
+                placement_height = 1200.0;
+                placement_width = 1920.0;
                 rotation = 0.0;
                 type = "clock";
 
                 settings = {
                   background = false;
                   center_text = true;
-                  color = "secondary";
+                  color = "primary";
                   font_family = config.custom.fonts.monospace;
-                  format = "{:%H:%M:%S}";
+                  format = "{:%H:%M}";
                 };
               };
             };
