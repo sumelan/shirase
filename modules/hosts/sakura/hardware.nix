6c3e66c326150061c43841e00d7fe0d3ccd7738f
sumelan <sumelan@proton.me>
2026-09-25 23:34:11 +0900

    remove sakura overlays
    Signed-off-by: sumelan <sumelan@proton.me>

--- modules/hosts/sakura/hardware.nix
+++ modules/hosts/sakura/hardware.nix
@@ -15,20 +15,5 @@
 
     nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
     hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-
-    # https://github.com/NixOS/nixpkgs/issues/562919
-    nixpkgs.overlays = [
-      (final: prev: {
-        linux-firmware = prev.linux-firmware.overrideAttrs (_: {
-          version = "20260810";
-          src = final.fetchFromGitLab {
-            owner = "kernel-firmware";
-            repo = "linux-firmware";
-            tag = "20260810";
-            hash = "sha256-P/fPpqaatp8Z2GV+I/OChiWGn6AhV+8w1RMFuX/LqHc=";
-          };
-        });
-      })
-    ];
   };
 }
